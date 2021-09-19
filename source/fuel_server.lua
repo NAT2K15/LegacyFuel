8c0ec8732b331343f422a194c2f73e8ec3a193dc
NAT2K15 <72643615+NAT2K15@users.noreply.github.com>
2021-09-18 22:17:23 -0400

    Fixed money issues

--- source/fuel_server.lua
+++ source/fuel_server.lua
@@ -3,9 +3,10 @@ if Config.UseMoney then
 	AddEventHandler('fuel:pay', function(price)
 		local src = source
 		local xPlayer = exports.money:getaccount(src)
-
-		if price > 0 then
-			exports.money:updateaccount(src, {cash = xPlayer.amount, bank = xPlayer.bank - price})
+		local fix =  xPlayer.bank - tonumber(price)
+		print(price, fix)
+		if tonumber(price) > 0 then
+			exports.money:updateaccount(src, {cash = xPlayer.amount, bank = xPlayer.bank - fix})
 		end
 	end)
 end
