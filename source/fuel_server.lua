ddba233ad8571f55015fa7450c7d293118c9f5e7
NAT2K15 <72643615+NAT2K15@users.noreply.github.com>
2021-09-19 11:01:29 -0400

    Update fuel_server.lua

--- source/fuel_server.lua
+++ source/fuel_server.lua
@@ -4,9 +4,8 @@ if Config.UseMoney then
 		local src = source
 		local xPlayer = exports.money:getaccount(src)
 		local fix =  xPlayer.bank - tonumber(price)
-		print(price, fix)
 		if tonumber(price) > 0 then
-			exports.money:updateaccount(src, {cash = xPlayer.amount, bank = xPlayer.bank - fix})
+			exports.money:updateaccount(src, {cash = xPlayer.amount, bank = fix})
 		end
 	end)
 end
